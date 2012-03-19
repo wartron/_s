34e5cff795e17ad0957cfce898dbd13cdf6636aa
willwharton <will@wideopentech.com>
2012-03-19 15:06:39 -0400

    fork overwrite check

--- fork.sh
+++ fork.sh
@@ -2,12 +2,31 @@
 echo "forking _s"
 echo "new name: $1"
 echo "path to wp themes: $2"
+
 SFORK_PATH="$2/$1"
-echo "making: $SFORK_PATH"
-mkdir $SFORK_PATH
+#maybe check if a wp-conent/themes/ exists and select that dir so the base can be the root wp install
+
+
+SFORK_RESPONSE=
+if [ -d $SFORK_PATH ]; then
+	echo -n "Output folder exists. Overwrite? (y/n) > "
+	read SFORK_RESPONSE
+	if [ "$SFORK_RESPONSE" != "y" ]; then
+		echo "Exiting"
+		exit 1
+	fi
+else
+	echo "making: $SFORK_PATH"
+	mkdir $SFORK_PATH
+fi
+
+
+
+echo "copying _S to $SFORK_PATH"
 cp -rf ./_s/* $SFORK_PATH/
 
 
+echo "replacing _s to $1_s in all files"
 #replace _s_ with $1_s_ in all files
 #there is probably a more bash-foo way of doing this, but it works.
 sed -i "s/: _s/: $1_s/g" $SFORK_PATH/style.css #for theme name
@@ -22,3 +41,5 @@ sed -i "s/'_s'/'$1_s'/g" $SFORK_PATH/inc/*.php
 
 sed -i "s/_s_/$1_s_/g" $SFORK_PATH/inc/theme-options/*.php
 sed -i "s/'_s'/'$1_s'/g" $SFORK_PATH/inc/theme-options/*.php
+
+echo "done!"
