3b9f58ed7b7d11d3787434ddd72ea2d048822121
willwharton <will@wideopentech.com>
2012-03-19 14:49:30 -0400

    fork now finds __( strings

--- fork.sh
+++ fork.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "forking _s"
 echo "new name: $1"
-echo "path: $2"
+echo "path to wp themes: $2"
 SFORK_PATH="$2/$1"
 echo "making: $SFORK_PATH"
 mkdir $SFORK_PATH
@@ -10,7 +10,15 @@ cp -rf ./_s/* $SFORK_PATH/
 
 #replace _s_ with $1_s_ in all files
 #there is probably a more bash-foo way of doing this, but it works.
-sed -i "s/_s_/$1_s_/g" $SFORK_PATH/*.php
-sed -i "s/: _s/: $1_s/g" $SFORK_PATH/style.css
+sed -i "s/: _s/: $1_s/g" $SFORK_PATH/style.css #for theme name
+
+sed -i "s/_s_/$1_s_/g" $SFORK_PATH/*.php #for function calls
+sed -i "s/'_s'/'$1_s'/g" $SFORK_PATH/*.php #for __( strings
+
+
 sed -i "s/_s_/$1_s_/g" $SFORK_PATH/inc/*.php
+sed -i "s/'_s'/'$1_s'/g" $SFORK_PATH/inc/*.php
+
+
 sed -i "s/_s_/$1_s_/g" $SFORK_PATH/inc/theme-options/*.php
+sed -i "s/'_s'/'$1_s'/g" $SFORK_PATH/inc/theme-options/*.php
